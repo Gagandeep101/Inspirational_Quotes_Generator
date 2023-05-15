62469b7cfcbd6443737f990ea8dfc5fc13b16f0a
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-15 12:45:20 +0530

    refactor : using fetch to collect the data from api in js

--- brain.js
+++ brain.js
@@ -6,3 +6,29 @@ var author = document.querySelector(".author");
 
 var main_scr=document.querySelector(".main_scr");
 
+function quotes_from_api() {
+    
+    var data = fetch("https://type.fit/api/quotes");
+
+    data.then((value) => {
+        return value.json();
+    }).then((value) => {
+
+        var ran_no = Math.floor(Math.random() * (1642 - 0) + 0);
+
+        if (value[ran_no].author === null && value[ran_no].text !== null) {
+            text.innerText = "\""+value[ran_no].text+"\"";
+            author.innerText = "No author";
+        }
+
+        else if (value[ran_no].text === null && value[ran_no].author !== null) {
+            quotes_from_api();
+            return;
+        }
+
+        else {
+            text.innerText ="\""+ value[ran_no].text +"\"";
+            author.innerText = "\""+value[ran_no].author+"\"";
+        }
+    })
+}
