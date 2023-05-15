e4c0ffa5badd23e16dde052e59dacffd47ecb8c5
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-15 12:44:15 +0530

    refactor : declare basic variable in js

---
+++ brain.js
@@ -0,0 +1,8 @@
+var gen_quotes_btn = document.querySelector(".gen_quotes");
+
+var text = document.querySelector(".quotes");
+
+var author = document.querySelector(".author");
+
+var main_scr=document.querySelector(".main_scr");
+
